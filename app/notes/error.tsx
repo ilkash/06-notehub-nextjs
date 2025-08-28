c487629a36440834d0bea73a228b0c30db406bd5
ilkash <126604951+ilkash@users.noreply.github.com>
2025-08-28 11:19:04 +0300

    update my error

--- app/notes/error.tsx
+++ app/notes/error.tsx
@@ -7,8 +7,7 @@ type Props = {
 const Error = ({ error }: Props) => {
   return (
     <div>
-      <h2>Помилка при завантаженні</h2>
-      <p>{error.message}</p>
+      <p>Could not fetch the list of notes. {error.message}</p>
     </div>
   );
 };
--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -4,7 +4,7 @@ import {
   HydrationBoundary,
   QueryClient,
 } from "@tanstack/react-query";
-import NotesClient from "./NotesClient/Notes.client";
+import NotesClient from "./Notes.client";
 
 export default async function Notes() {
   const queryClient = new QueryClient();
