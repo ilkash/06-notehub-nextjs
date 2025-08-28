b0de462b4d67df560dbf776b0fa354428d96639e
ilkash <126604951+ilkash@users.noreply.github.com>
2025-08-28 11:07:06 +0300

    Update NoteDetails.client.tsx

--- app/notes/[id]/NoteDetails.client.tsx
+++ app/notes/[id]/NoteDetails.client.tsx
@@ -4,16 +4,17 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import css from "./NoteDetails.client.module.css";
 export default function NoteDetailsClient() {
-  const params = useParams<{ id: string }>();
-  const id = params?.id ? Number(params.id) : null;
+  const { id } = useParams();
+
   const {
     data: note,
     isLoading,
     error,
   } = useQuery({
     queryKey: ["dateils", { id: id }],
-    queryFn: () => fetchNoteById(String(id)),
+    queryFn: () => fetchNoteById(id as string),
     enabled: !!id,
+    refetchOnMount: false,
   });
   if (isLoading) {
     return <p>Loading, please wait...</p>;
@@ -21,6 +22,10 @@ export default function NoteDetailsClient() {
   if (error || !note) {
     return <p>Something went wrong.</p>;
   }
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
+
   return (
     <div>
       <div className={css.container}>
@@ -29,7 +34,7 @@ export default function NoteDetailsClient() {
             <h2>{note.title}</h2>
           </div>
           <p className={css.content}>{note.content}</p>
-          <p className={css.date}>{note.createdAt}</p>
+          <p className={css.date}>{formattedDate}</p>
         </div>
       </div>
     </div>
